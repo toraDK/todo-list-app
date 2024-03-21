95b4e9a96ae812a602284a1cf9fdbb2c74353c3b
tora180405 <toradigda@gmail.com>
2024-03-21 20:09:30 +0700

    fix index.blade view

--- resources/views/layouts/index/index.blade.php
+++ resources/views/layouts/index/index.blade.php
@@ -10,8 +10,10 @@
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
+    <link rel="stylesheet" href="/css/index.css">
+
     <style>
-        .bd-placeholder-img {
+        .bd-placeholder-img {       
             font-size: 1.125rem;
             text-anchor: middle;
             -webkit-user-select: none;
